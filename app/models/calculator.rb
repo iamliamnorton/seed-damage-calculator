c1b211a220fc911ede9d888f970a2c94a0262eed
Liam Norton <iamliamnorton@gmail.com>
2012-04-26 17:20:35 +1000

    Refactored controller code in Calculator, moved functionality into model

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -3,17 +3,27 @@ class Calculator
   include ActiveModel::Conversion
   extend ActiveModel::Naming
   
-  attr_accessor :crop_id, :fertiliser_id, :soil_moisture_id, :soil_texture_id, :seed_furrow_opening_width, :row_spacing, :tolerated_stand_loss
+  attr_accessor :crop_id, :fertiliser_id, :soil_moisture_id, :soil_texture_id, 
+                :seed_furrow_opening_width, :row_spacing, :tolerated_stand_loss
   
   validates :seed_furrow_opening_width, :row_spacing, :tolerated_stand_loss, presence: true, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 100 }
   
   def initialize(attributes = {})
-      attributes.each do |name, value|
-        send("#{name}=", value)
-      end
+    attributes.each do |name, value|
+      send("#{name}=", value)
     end
+  end
 
-    def persisted?
-      false
-    end
+  def persisted?
+    false
+  end
+  
+  def calculate_result
+    regression_coefficient = RegressionCoefficient.where(:crop_id => self.crop_id, :fertiliser_id => self.fertiliser_id).first.value
+    soil_coefficient = SoilCoefficient.where(:soil_moisture_id => self.soil_moisture_id, :soil_texture_id => self.soil_texture_id).first.value
+    numerator = 30 * self.seed_furrow_opening_width.to_i * -1 * self.tolerated_stand_loss.to_i
+    denominator = regression_coefficient * self.row_spacing.to_i * soil_coefficient
+    return numerator / denominator
+    
+  end
 end
