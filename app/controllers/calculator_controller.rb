76c998dd162e251b8626426fa30f25dea71c5f8d
Liam Norton <iamliamnorton@gmail.com>
2012-06-04 20:24:52 +1000

    Bug fix heroku locale

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -12,17 +12,17 @@ class CalculatorController < ApplicationController
     @soil_textures = SoilTexture.all
 
     if request.post? && params[:calculator]
-      respond_to do |format|
-        if @calculator.valid?
-          format.html { redirect_to root_path(:calculator => params[:calculator]), notice: 'Calculation was successfully completed.' }
-        else
-          # This will have to be redone, need a better way of showing errors in the calculator form
-          errors = ''
-          @calculator.errors.full_messages.each do |msg|
-            errors += msg + "\n"
-          end
-          format.html { redirect_to calculator_path(:calculator => params[:calculator]), alert: "Calculation was not successfully completed. #{errors}" }
+      if @calculator.valid?
+        flash[:notice] = 'Calculation was successfully completed.'
+        redirect_to calculator_path(:calculator => params[:calculator])
+      else
+        # This will have to be redone, need a better way of showing errors in the calculator form
+        errors = ''
+        @calculator.errors.full_messages.each do |msg|
+          errors += msg + "\n"
         end
+        flash[:alert] = "Calculation was not successfully completed. #{errors}"
+        redirect_to calculator_path(:calculator => params[:calculator])
       end
     end
   end
