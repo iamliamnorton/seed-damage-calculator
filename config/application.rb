2fc48a3c6e39724992d2d4f91ff6bcc243bb4caa
Liam Norton <iamliamnorton@gmail.com>
2013-01-23 19:12:52 +1100

    Bug fix for heroku

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module Fasdac
       g.fixture_replacement :machinist
     end
 
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
