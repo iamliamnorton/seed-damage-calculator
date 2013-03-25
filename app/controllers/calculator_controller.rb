89d1cb9edf2d9956623badfb351982425315a6f8
Liam Norton <iamliamnorton@gmail.com>
2013-03-25 20:30:42 +1100

    Better layout for calculator controller

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,30 +1,26 @@
 class CalculatorController < ApplicationController
   before_filter :load_vars
 
-  def load_vars
-    @crops = Crop.all
-    @fertilisers = Fertiliser.all
-    @soil_moistures = SoilMoisture.all
-    @soil_textures = SoilTexture.all
-  end
-
   def index
-    redirect_to calculator_path(:locale => params[:set_locale], :calculator => params[:calculator]) if params[:set_locale]
+    return redirect_to calculator_path(:locale => params[:set_locale], :calculator => params[:calculator]) if params[:set_locale]
 
     @calculator = Calculator.new(params[:calculator])
 
     if request.post? && params[:calculator]
       if @calculator.valid?
-        respond_to do |format|
-          format.html { redirect_to calculator_path(:calculator => params[:calculator]), :notice => 'Calculation was successfully completed.' }
-          format.js
-        end
+        redirect_to calculator_path(:calculator => params[:calculator]), :notice => 'Calculation was successfully completed.'
       else
-        respond_to do |format|
-          format.html { redirect_to calculator_path(:calculator => params[:calculator]), :alert => "Calculation was not successfully completed. #{@calculator.errors.full_messages.to_sentence}" }
-          format.js
-        end
+        redirect_to calculator_path(:calculator => params[:calculator]), :alert => "Calculation was not successfully completed. #{@calculator.errors.full_messages.to_sentence}"
       end
     end
+  end
+
+  private
+
+  def load_vars
+    @crops = Crop.all
+    @fertilisers = Fertiliser.all
+    @soil_moistures = SoilMoisture.all
+    @soil_textures = SoilTexture.all
   end
 end
