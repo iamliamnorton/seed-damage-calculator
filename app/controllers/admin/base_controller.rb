62977d2f59ec3b8a927b561a8d940380536c595f
Liam Norton <lnorton@zendesk.com>
2018-12-29 21:30:03 +1100

    No need to auth in dev

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,19 +1,12 @@
-require 'digest'
-
 class Admin::BaseController < ApplicationController
   before_action :authenticate
 
   def authenticate
+    return unless Rails.env.production?
+
     authenticate_or_request_with_http_basic('Administration') do |username, password|
       session[:admin] = true
-      md5_of_password = Digest::MD5.hexdigest(password)
-      if Rails.env == "production"
-        #staging & production server code
-        username == ENV['APP_USERNAME'] && password == ENV['APP_PASSWORD']
-      else
-        #development server code
-        username == 'admin' && md5_of_password == '21232f297a57a5a743894a0e4a801fc3'
-      end
+      username == ENV['APP_USERNAME'] && password == ENV['APP_PASSWORD']
     end
   end
 end
